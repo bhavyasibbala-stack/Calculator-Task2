7eff1b22c0295b7ca784b36b0be23ce11ee3f1bd
bhavyasibbala-stack <bhavyasibbala@gmail.com>
2026-09-24 20:17:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value = display.value + value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let result = eval(display.value);
+        display.value = result;
+    } catch {
+        display.value = "Error";
+    }
+}
